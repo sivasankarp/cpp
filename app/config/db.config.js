9107ba1564a195d905b47402d7aa2176f7d82503
Sivasankar Ponnurangam <Sivasankar.Ponnurangam@gilead.com>
2022-03-02 07:32:24 +0000

    updated db#

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cpp"
+host: "db-ross-mysql.c9i4uswfa97s.us-west-2.rds.amazonaws.com",
+user: "admin",
+password: "terraform123#",
+database: "ross-api"
 });
 
 // open the MySQL connection
