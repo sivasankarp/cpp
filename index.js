3beb83cb66fcbaf7af4b7840649315e2fd00346d
siva <sivaforum@gmail.com>
2022-03-06 12:23:54 +0000

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var corsOptions = {
 
 var allowCrossDomain=function(req,res,next){
   res.header('Access-Control-Allow-Origin',"*");
-  res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS' );
   res.header('Access-Control-Allow-Headers','Content-Type');
   next();
 };
