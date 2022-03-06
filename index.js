227b146d3d400ccb6c7431d3cea64de0d991d0e9
siva <sivaforum@gmail.com>
2022-03-06 03:02:54 +0000

    fixed cors issue
    fixed cors issue

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ var corsOptions = {
   origin: "http://35.178.91.217:3003/"
 };
 
+var allowCrossDomain=function(req,res,next){
+  res.header('Access-Control-Allow-Origin',"*");
+  res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers','Content-Type');
+  next();
+};
+
 app.use(cors());
 
 // parse requests of content-type: application/json
@@ -15,6 +22,7 @@ app.use(bodyParser.json());
 
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(allowCrossDomain);
 
 // simple route
 app.get("/", (req, res) => {
